0a3386d784b18c570db4baf038e7ea2585c8d1f5
Pratiksha <pratikshadadhwal842@gmail.com>
2025-05-12 16:57:03 +0530

    Here is the Day3

---
+++ STL/main.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+// // Pairs
+// pair<int,int>p={5,3};
+// cout<<p.first<<" "<<p.second;
+// cout<<endl;
+
+// pair<int,int> arr[]={{1,2},{3,4},{4,5}};
+// cout<<arr[1].first;
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// vectors
+// vector<int>v;
+// v.push_back(5);
+// v.push_back(4);
+// v.push_back(3);
+// v.push_back(2);
+// v.push_back(1);
+
+// // for(int i=0;i<v.size();i++){
+// //     cout<<v[i];
+// //     cout<<" ";
+// // }
+// // vector<int>::iterator it=v.begin();
+// // vector<int>::iterator it=v.end();
+// // cout<<*(it-1);
+
+// // for(auto it=v.begin();it!=v.end();it++){
+// //     cout<<*it<<" ";
+// // }
+// vector<int>::reverse_iterator it=v.rend();
+// // vector<int>::reverse_iterator it=v.rbegin();
+// v.erase(v.begin()+1,v.begin()+3);
+// for(auto it=v.begin();it!=v.end();it++){
+//     cout<<*it<<" ";
+// }
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// // List
+// list<int>ls;
+// ls.push_back(2);
+// ls.push_back(3);
+// ls.push_front(1);
+
+// for(auto it=ls.begin();it!=ls.end();it++){
+//     cout<<*it<<" ";
+// }
+
+// // remaining functions are same as in vectors
+
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Deque
+// deque<int>dq;
+// dq.push_back(3);
+// dq.push_back(3);
+// dq.push_back(3);
+// dq.push_back(3);
+
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Stack
+stack<int>s;
+// FIFO
+
+
+// Queue
+queue<int>q;
+
+
+// Priority Queue
+priority_queue<int>pq;
+pq.push(3);
+pq.push(13);
+pq.push(30);
+pq.push(33);
+pq.pop();
+
+
+
+// Set
+set<int>s;
+
+
+
+
+
+    return 0;
+}
